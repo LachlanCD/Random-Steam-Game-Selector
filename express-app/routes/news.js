c11ad7a7d1163bab7dfab5047bdd1aada472a9b7
Lachlan Garrahy <lachikent@gmail.com>
2023-08-29 00:41:27 +1000

    dockerised

--- express-app/routes/news.js
+++ express-app/routes/news.js
@@ -1,16 +1,13 @@
 // api key 792e9726768442e2abdd34239b0337be
 
-const NEWS_API_KEY = process.env.NEWS_API_KEY;
-
 const express = require('express');
 const axios = require('axios');
 const router = express.Router();
 
 /* GET home page. */
 router.get('/:query', async function(req, res, next) {
-    const options = createNewsOptions(req.params.query);
-    const url = `https://${options.hostname}${options.path}`;
-    console.log(url)
+    const NEWS_API_KEY = process.env.NEWS_API_KEY;
+    const url = `https://newsapi.org/v2/everything?q=${req.params.query}&sortBy=popularity&pageSize=100&apiKey=${NEWS_API_KEY}`;
 
     try{
         const vidList = await axios.get(url);
@@ -21,29 +18,5 @@ router.get('/:query', async function(req, res, next) {
         console.log(error)
     }
 });
-
-const news = {
-    sortBy: 'popularity',
-    pageSize: '100',
-    api_key: "792e9726768442e2abdd34239b0337be",
-};
-
-function createNewsOptions(query) {
-
-    const options = {
-        hostname: 'newsapi.org',
-        port: 443,
-        path: '/v2/everything?',
-        method: 'GET'
-    }
-
-    const str = 'q=' + query+
-        '&sortBy=' + news.sortBy +
-        '&pageSize=' + news.pageSize +
-        '&apiKey=' + news.api_key;
-
-    options.path += str;
-    return options;
-}
 
 module.exports = router;
--- express-app/routes/youtube.js
+++ express-app/routes/youtube.js
@@ -1,4 +1,4 @@
-const YOUTUBE_API_KEY = process.env.YOUTUBE_API_KEY;
+//AIzaSyAJxzcsGj_2q3mtYT3I61W4EUy_vdlHER0
 
 const express = require('express');
 const axios = require('axios');
@@ -6,12 +6,11 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/:query', async function(req, res, next) {
-    const options = createYTOptions(req.params.query);
-    const url = `https://${options.hostname}${options.path}`;
+    const YOUTUBE_API_KEY = process.env.YOUTUBE_API_KEY;
+    const url = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${req.params.query}&key=${YOUTUBE_API_KEY}`;
 
     try{
         const vidList = await axios.get(url);
-        console.log(vidList.data)
 
         res.json(vidList.data);
 
@@ -19,29 +18,5 @@ router.get('/:query', async function(req, res, next) {
         console.log(error)
     }
 });
-
-const youtube = {
-    part: 'snippet',
-    results: '25',
-    api_key: "AIzaSyAJxzcsGj_2q3mtYT3I61W4EUy_vdlHER0",
-};
-
-function createYTOptions(query) {
-
-    const options = {
-        hostname: 'youtube.googleapis.com',
-        port: 443,
-        path: '/youtube/v3/search?',
-        method: 'GET'
-    }
-
-    const str = 'part=' + youtube.part +
-        '&maxResults=' + youtube.results +
-        '&q=' + query+
-        '&key=' + youtube.api_key;
-
-    options.path += str;
-    return options;
-}
 
 module.exports = router;
